4ce2f6c4a5981234626a1bb758e5a0321085c125
Wonyoung Ju <getogrand@gmail.com>
2017-07-30 21:32:40 +0900

    Fix: compose can receive only functions

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,14 @@ import { routerMiddleware } from 'react-router-redux'
 import reducers from '../reducers'
 import history from '../history'
 
-export default createStore(
-  reducers,
+const enhancer = process.env.NODE_ENV === 'production' ?
+  applyMiddleware(routerMiddleware(history)) :
   compose(
     applyMiddleware(routerMiddleware(history)),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
+  )
+
+export default createStore(
+  reducers,
+  enhancer,
 )
